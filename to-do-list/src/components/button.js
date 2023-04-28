5ba929423426f1b2062e24aee9d5ffa7f53f4bfa
Diego Barletta <diegohbarletta@gmail.com>
2023-04-27 21:58:47 -0300

    primer commit del proyecto-prueba react-bootstrap

---
+++ to-do-list/src/components/button.js
@@ -0,0 +1,14 @@
+import Button from 'react-bootstrap/Button';
+export default function Boton(){
+    return(
+<>
+  <Button as="a" variant="primary">
+    vamo lo pibe componente
+  </Button>
+  <Button as="a" variant="success">
+    Button as link
+  </Button>
+</>
+    )
+
+}
